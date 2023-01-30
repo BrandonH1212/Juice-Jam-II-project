2be584a1e5f066cb61a5aa7e92401bad1d7952c0
EMdT <gccpsben@gmail.com>
2023-01-31 03:26:32 +0800

    Added Prefabs

--- Assets/Scripts/CircluarSpawner.cs
+++ Assets/Scripts/CircluarSpawner.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class CircluarSpawner : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
     public int Interval = 8;
-    public GameObject ProjectileParent;
 
     // Start is called before the first frame update
     void Start()
     {
-        var projectileStats = ProjectileParent.GetComponent<Projectile>().GetStatsAppliedAsDictionary();
         if (ObjectToSpawn != null)
         {
             for (float i = 0; i < 2f * Mathf.PI; i += (2f * Mathf.PI) / Interval)
@@ -19,9 +18,10 @@ public class CircluarSpawner : MonoBehaviour
                 var newObject = Instantiate(ObjectToSpawn);
                 newObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
                 newObject.transform.rotation = Quaternion.EulerRotation(new Vector3(0,0, i));
-                newObject.GetComponent<Projectile>().ApplyStats(projectileStats);
             }
         }
+
+        GetComponent<Projectile>().ApplyStats(GetComponent<Projectile>().GetStatsAppliedAsDictionary());
     }
 
     // Update is called once per frame
